3b96fdbbd285669dea8e9d08d175fa55f2dd9cd3
Fujun Du <fujun.du@gmail.com>
2020-12-18 00:18:32 +0800

    typo corrected

--- src/ray_tracing.f90
+++ src/ray_tracing.f90
@@ -459,7 +459,7 @@ subroutine integerate_a_ray(ph, tau, Nup, Nlow, is_line)
         !
         Nup  = Nup  + (mole_exc%p%density_mol * length * phy_AU2cm) * yup
         Nlow = Nlow + (mole_exc%p%density_mol * length * phy_AU2cm) * ylow
-        if ((ylow .ge. 1.1D0) .or. (yup .ge. 1.1D0). or. (Nup .ge. 1.0D30) .or. (Nlow .ge. 1.0D30)) then
+        if ((ylow .ge. 1.1D0) .or. (yup .ge. 1.1D0) .or. (Nup .ge. 1.0D30) .or. (Nlow .ge. 1.0D30)) then
           write(*, *) 'Abnormal level occupation and/or column densities!'
           write(*, *) 'yup, ylow, Nup, Nlow, length, mole_exc%p%density_mol, phy_AU2cm, mole_exc%p%dv:'
           write(*,*) yup, ylow, Nup, Nlow, length, mole_exc%p%density_mol, phy_AU2cm, mole_exc%p%dv
@@ -1257,8 +1257,8 @@ subroutine set_using_mole_params(mole, c)
   end select
   if (c%abundances(mole%iSpe) .ge. 1.0D1) then
     write(*,*) 'Abnormal abundance!'
-    write(*,*) 'iSpe, c%abundances(mole%iSpe):'
-    write(*,*) iSpe, c%abundances(mole%iSpe)
+    write(*,*) 'mole%iSpe, c%abundances(mole%iSpe):'
+    write(*,*) mole%iSpe, c%abundances(mole%iSpe)
   end if
   !
   mole%density_mol = mole%density_mol * mole%abundance_factor
